51302a688ae4f47bbc73b82b64970a5935d0ac43
Dan Milliron <dan@railsblazer.com>
2013-08-26 13:12:58 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MishDinners::Application.config.secret_key_base = '2738e0383adf10160571fb3e7483482c2d39d890b092c6fa902b0a66281802afd652a1c04887f5ee7886797ebac08c896a207f7ed22101822fae3e5242fb21d7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MishDinners::Application.config.session_store :cookie_store, key: '_mish-dinners_session'
