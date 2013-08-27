b75e34be073e14c01609f79e0577c300452cbea8
Dan Milliron <dan@railsblazer.com>
2013-08-27 10:08:31 -0700

    Code refactor; no functional change

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,17 +1,13 @@
 class CalendarsController < ApplicationController
-  before_action :set_unit, only: [:show]
+  before_action :set_unit,         only: [:show]
+  before_action :set_first_sunday, only: [:show]
 
   def index
     redirect_to '/lo/lo'
   end
 
   def show
-    today = Date.today
-    first_sunday = today - today.wday
-    first_sunday += 7 if params.has_key?(:next)
-    first_sunday -= 7 if params.has_key?(:prev)
-
-    @calendar = Calendar.new(@unit, first_sunday, for_print: params.has_key?(:print))
+    @calendar = Calendar.new(@unit, @first_sunday, for_print: params.has_key?(:print))
   end
 
   private
@@ -19,5 +15,12 @@ class CalendarsController < ApplicationController
   def set_unit
     division = Division.find_by_abbr(params[:division_abbr])
     @unit    = division.units.find_by_abbr(params[:unit_abbr])
+  end
+
+  def set_first_sunday
+    today         = Date.today
+    @first_sunday = today - today.wday
+    @first_sunday += 7 if params.has_key?(:next)
+    @first_sunday -= 7 if params.has_key?(:prev)
   end
 end
