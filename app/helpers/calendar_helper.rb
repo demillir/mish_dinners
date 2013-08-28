f93f77d37d28d3c60666114dde55fc770f4153a0
Dan Milliron <dan@railsblazer.com>
2013-08-28 08:12:05 -0700

    Truncated fields are now abbrs

--- app/helpers/calendar_helper.rb
+++ app/helpers/calendar_helper.rb
@@ -1,6 +1,10 @@
 module CalendarHelper
   def fixed_size(text, truncate_at)
     text_str = text.to_s
-    text_str.truncate(truncate_at)
+    return text_str if text_str.length <= truncate_at
+
+    content_tag(:abbr, title: text_str) do
+      text_str.truncate(truncate_at)
+    end
   end
 end
