cb33f4b7236fcdcbd519b7005afdeb9c1a8780a3
Dan Milliron <dan@railsblazer.com>
2013-08-28 13:14:47 -0700

    Code improvement; no functional change

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -48,6 +48,6 @@ class Unit < ActiveRecord::Base
   private
 
   def column_list(column)
-    Appointment.joins(:day).where(:days => {:unit_id => self.id}).order(column).uniq.pluck(column).find_all(&:present?)
+    Appointment.joins(:day).merge(Day.where(:unit_id => self.id)).order(column).uniq.pluck(column).find_all(&:present?)
   end
 end
