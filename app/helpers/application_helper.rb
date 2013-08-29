21da459ced939352458ee5d05a3167d2e76992cb
Dan Milliron <dan@railsblazer.com>
2013-08-28 18:26:34 -0700

    Made the meal name configurable

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   # Returns a string like "an in-home dinner"
   def meal_name_with_indefinite_article(appointment)
-    meal_name = [@appointment.css_class, "dinner"].join(' ')
+    meal_name = [appointment.meal_type, Figaro.env.meal_name].join(' ')
     meal_name.with_indefinite_article
   end
 end
