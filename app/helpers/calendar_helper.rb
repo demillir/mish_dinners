e3dbe29f21bab79b1c413f528a2ac059cee59354
Dan Milliron <dan@railsblazer.com>
2013-08-29 23:58:45 -0700

    Added navigation arrows to the calendar pages

--- app/helpers/calendar_helper.rb
+++ app/helpers/calendar_helper.rb
@@ -24,4 +24,20 @@ module CalendarHelper
       "#{calendar.start_date.strftime('%b %Y')} - #{calendar.end_date.strftime('%b %Y')}"
     end
   end
+
+  def adjacent_calendar_path(calendar, multiplier)
+    days_in_calendar = calendar.end_date - calendar.start_date + 1
+    new_calendar_start_date = calendar.start_date + (multiplier * days_in_calendar)
+
+    uri = URI(request.original_url)
+    date_query = {date: new_calendar_start_date}.to_query
+    if uri.query.blank?
+      uri.query = date_query
+    elsif (old_date_query = uri.query.split('&').grep(/\Adate=\d/).first)
+      uri.query = uri.query.split(old_date_query, -1).join(date_query)
+    else
+      uri.query = [uri.query, date_query].join('&')
+    end
+    uri.to_s
+  end
 end
