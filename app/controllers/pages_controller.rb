6eb76727c4673aad45c125a6b7af826e7e50239c
Dan Milliron <dan@railsblazer.com>
2013-08-31 14:45:05 -0700

    Converted a hard-coded path to url_for

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
   def home
-    redirect_to '/lo/lo'
+    redirect_to url_for(controller: :calendars, action: :show, division_abbr: 'lo', unit_abbr: 'lo')
   end
 end
