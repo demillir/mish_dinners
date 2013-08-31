ef8d6034fc9beb0fca51c36aa1b898c0b6312ca2
Dan Milliron <dan@railsblazer.com>
2013-08-31 14:35:24 -0700

    Converted a hard-coded url interpolator to parameterized

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,7 @@ module MishDinners
 
     # We cannot accommodate more than six recipients with a single-page calendar.
     config.max_recipients = 6
+
+    config.action_mailer.default_url_options = { host: Figaro.env.app_domain }
   end
 end
