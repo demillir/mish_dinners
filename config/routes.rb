7808f23e590bb005952a3c54061aee781b1f85b0
Dan Milliron <dan@railsblazer.com>
2013-08-31 14:36:53 -0700

    Configured the rails/info routes, given the catch-all routes we have

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,12 @@ MishDinners::Application.routes.draw do
   resources :calendars, :only => [:show, :edit, :update]
   resources :settings,  :only => [:edit, :update]
 
+  if Rails.env.development?
+    get '/rails/info/properties' => "rails/info#properties"
+    get '/rails/info/routes'     => "rails/info#routes"
+    get '/rails/info'            => "rails/info#index"
+  end
+
   get ':division_abbr/:unit_abbr/:uuid' => 'calendars#edit'
   get ':division_abbr/:unit_abbr'       => 'calendars#show'
 end
