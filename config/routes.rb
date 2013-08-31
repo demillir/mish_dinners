5497015f29e1c6a4ec2be70a4f0ac442b169cbc1
Dan Milliron <dan@railsblazer.com>
2013-08-31 14:52:33 -0700

    Removed an obsolete route

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,6 @@ MishDinners::Application.routes.draw do
   end
 
   get ':division_abbr/:unit_abbr/print' => 'calendars#print'
-  get ':division_abbr/:unit_abbr/:uuid' => 'calendars#edit'
   get ':division_abbr/:unit_abbr/edit'  => 'calendars#edit'
   get ':division_abbr/:unit_abbr'       => 'calendars#show'
 end
